89581038535fbbaa100d701e1268dea529b95282
StephanieZou1 <stephanie.zou3@gmail.com>
2020-07-12 11:28:55 -0400

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -66,7 +66,7 @@ export default function SimpleTabs() {
             </Tabs>
         </AppBar>
         <NavBar value={value} index={0}>
-            Home
+            {/* Home */}
         </NavBar>
         <NavBar value={value} index={1}>
             Meet the Team
--- src/containers/HomeContainer.jsx
+++ src/containers/HomeContainer.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -28,6 +32,23 @@ const useStyles = makeStyles((theme) => ({
             </Grid>
             <Grid item xs={6}>
               <img className="home-image" src={require("../images/home_1.jpg")}/>
+            </Grid>
+            <Grid item xs={12}>
+            <Container maxWidth="100%">
+                <Typography component="div" style={{ backgroundColor: '#f9f9f9', height: '5rem' }} />
+                <Grid container spacing={3}>
+                    <Grid item xs>
+                    <Paper className={classes.paper}>Surgical Masks</Paper>
+                    </Grid>
+                    <Grid item xs>
+                    <Paper className={classes.paper}>Gloves</Paper>
+                    </Grid>
+                    <Grid item xs>
+                    <Paper className={classes.paper}>Wipes and Disinfectants</Paper>
+                    </Grid>
+                </Grid> 
+                <Typography component="div" style={{ backgroundColor: '#f9f9f9', height: '5rem' }} />
+            </Container>
             </Grid>
           </Grid>
         </div>
