f22b279c7299fd51780369949628b18bfc63f7d4
StephanieZou1 <stephanie.zou3@gmail.com>
2020-07-11 23:55:34 -0400

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import SwipeableViews from 'react-swipeable-views';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import { Link } from 'react-router-dom'
 
 function NavBar(props) {
   const { children, value, index, ...other } = props;
@@ -15,8 +15,8 @@ function NavBar(props) {
     <div
       role="tabpanel"
       hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
       {value === index && (
@@ -36,62 +36,44 @@ NavBar.propTypes = {
 
 function a11yProps(index) {
   return {
-    id: `full-width-tab-${index}`,
-    'aria-controls': `full-width-tabpanel-${index}`,
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
   };
 }
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
-    width: 500,
   },
 }));
 
-export default function FullWidthTabs() {
+export default function SimpleTabs() {
   const classes = useStyles();
-  const theme = useTheme();
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  const handleChangeIndex = (index) => {
-    setValue(index);
-  };
-
   return (
-    <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="fullWidth"
-          aria-label="full width tabs example"
-        >
-          <Tab label="Home" {...a11yProps(0)} />
-          <Tab label="Meet the Team" {...a11yProps(1)} />
-          <Tab label="Contact Us" {...a11yProps(2)} />
-        </Tabs>
-      </AppBar>
-      <SwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={value}
-        onChangeIndex={handleChangeIndex}
-      >
-        <NavBar value={value} index={0} dir={theme.direction}>
-          Home
+        <div className={classes.root}>
+        <AppBar position="static" style={{ background: '#3f50b5', boxShadow: 'none'}}>
+            <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+            <Tab label="Home" component={Link} to="/"/>
+            <Tab label="Meet the Team" component={Link} to="/about" />
+            <Tab label="Contact Us" component={Link} to="/contact" />
+            </Tabs>
+        </AppBar>
+        <NavBar value={value} index={0}>
+            Home
         </NavBar>
-        <NavBar value={value} index={1} dir={theme.direction}>
-          Meet the Team
+        <NavBar value={value} index={1}>
+            Meet the Team
         </NavBar>
-        <NavBar value={value} index={2} dir={theme.direction}>
-          Contact Us
+        <NavBar value={value} index={2}>
+            Contact Us
         </NavBar>
-      </SwipeableViews>
-    </div>
+        </div>
   );
 }
--- src/containers/HomeContainer.jsx
+++ src/containers/HomeContainer.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-const HomeContainer = () => {
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
+  
+  export default function HomeContainer() {
+    const classes = useStyles();
+  
     return (
-        <div>
-            <h1>
-                hi
-            </h1>
+        <div className={classes.root}>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <Paper className={classes.paper}>xs=6</Paper>
+            </Grid>
+            <Grid item xs={6}>
+              <Paper className={classes.paper}>xs=6</Paper>
+            </Grid>
+          </Grid>
         </div>
-    )
-}
-
-export default HomeContainer
+      );
+    }
