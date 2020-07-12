e945c4f397556b80470eabe76bf9cbc51eb81cd5
StephanieZou1 <stephanie.zou3@gmail.com>
2020-07-11 20:30:47 -0400

    update

--- src/containers/HomeContainer.jsx
+++ src/containers/HomeContainer.jsx
@@ -1,13 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class HomeContainer extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+const HomeContainer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
 }
 
 export default HomeContainer
